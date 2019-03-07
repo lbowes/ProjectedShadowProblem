8f09fce55a974875528a2f11b3413e4d678726b6
Lewis Bowes <lmb573@its.york.ac.uk>
2019-03-07 17:30:05 +0000

    Structured project, internal/graphics

---
+++ src/internal/Envelop.java
@@ -0,0 +1,13 @@
+package internal;
+
+public class Envelop 
+{
+	private int leftBound;
+	private int height;
+	private int rightBound;
+	
+	public Envelop(int left, int height, int right)
+	{
+		
+	}
+}
