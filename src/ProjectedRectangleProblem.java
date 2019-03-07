81ad49e4ddf06115bd54ef3995c0dcd0855e30e8
Evan Snoswell <es1492@its.york.ac.uk>
2019-03-07 18:00:27 +0000

    Added two union algorithm skeletons
    Signed-off-by: Evan Snoswell <es1492@its.york.ac.uk>

--- src/ProjectedRectangleProblem.java
+++ src/ProjectedRectangleProblem.java
@@ -1,3 +1,4 @@
+import internal.Bar;
 
 class ProjectedRectangleProblem {
 
@@ -5,4 +6,13 @@ class ProjectedRectangleProblem {
 		System.out.println("Second commit");
 	}
 
+	public Envelop barsUnion(Bar bar1, Bar bar2)
+	{
+		return new Envelop();
+	}
+	
+	public Envelop barEnvelopUnion(Bar bar, Envelop envelop)
+	{
+		return new Envelop();
+	}
 }
