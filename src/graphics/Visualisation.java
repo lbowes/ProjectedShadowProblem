d8504c9ac2f325ed9971b5d7da35fc841a343a8c
Lewis Bowes <lmb573@its.york.ac.uk>
2019-03-07 17:53:01 +0000

    Added Visualisation class

---
+++ src/graphics/Visualisation.java
@@ -0,0 +1,10 @@
+package graphics;
+
+// - Responsible for observing the output of of the internal logic and presenting it to the user in some form
+// - Should not modify any internal state
+// - Purely functions as a layer on top of internal
+public class Visualisation {
+
+	Visualisation() { }	
+	
+}
