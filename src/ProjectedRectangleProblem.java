8694a0cce6f8c4eb4c326809bb82b837c6d4a43a
Evan Snoswell <es1492@its.york.ac.uk>
2019-03-07 18:06:53 +0000

    Moved algorithm to algorithm class
    Signed-off-by: Evan Snoswell <es1492@its.york.ac.uk>

--- src/ProjectedRectangleProblem.java
+++ src/ProjectedRectangleProblem.java
@@ -1,18 +1,9 @@
 import internal.Bar;
+import internal.Envelop;
 
 class ProjectedRectangleProblem {
 
 	public static void main(String[] args) {
 		System.out.println("Second commit");
-	}
-
-	public Envelop barsUnion(Bar bar1, Bar bar2)
-	{
-		return new Envelop();
-	}
-	
-	public Envelop barEnvelopUnion(Bar bar, Envelop envelop)
-	{
-		return new Envelop();
 	}
 }
