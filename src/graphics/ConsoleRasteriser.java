22e57f1e6843de38efcc44c6fcb6c5d5803d6376
Lewis Bowes <lmb573@its.york.ac.uk>
2019-03-07 18:37:29 +0000

    Added detail to visualisation

---
+++ src/graphics/ConsoleRasteriser.java
@@ -0,0 +1,23 @@
+package graphics;
+
+// Contains drawing logic used to display the output envelop
+public class ConsoleRasteriser {
+
+	// Internal state of the grid (some big block of characters, 1D array indexed as 2D?)
+	private  
+	
+	ConsoleRasteriser(int gridSizeX, int gridSizeY) {
+		
+	}
+	
+	void drawVerticalLine(int startX_grid, int startY_grid, int expanse_grid) 
+		// startX_grid is x coordinate of starting point of line
+		// startY_grid is y coordinate of starting point of line
+		// expanse_grid is either positive/negative and indicates how far the line goes up or down
+	{
+		// TODO: modifies the internal state of the character block
+		// This class will be responsible for storing the graphical representation, modified from above by Visualisation
+		// Once drawing is completed, the result can be extracted with toString() 
+	}
+	
+}
--- src/graphics/Visualisation.java
+++ src/graphics/Visualisation.java
@@ -1,10 +1,35 @@
 package graphics;
 
-// - Responsible for observing the output of of the internal logic and presenting it to the user in some form
+import java.util.ArrayList;
+
+import internal.Envelop;
+
+// - Responsible for observing the output of of the internal logic and presenting it to the user in some form (console output initially)
 // - Should not modify any internal state
 // - Purely functions as a layer on top of internal
 public class Visualisation {
-
-	Visualisation() { }	
+	
+	// Store the output array (indexed with 2D, but stored in 1D)
+	private String a = "";
+	
+	private ConsoleRasteriser mConsoleRasterizer;
+	
+	Visualisation(Envelop e) { 
+		assembleString(e);
+	}	
+	
+	public String toString() 
+		// Responsible for providing the entire console output as a single string
+	{
+		return mConsoleRasterizer.toString();
+	}
+	
+	private void assembleString(Envelop e) 
+		// Logic for building the output string from the input Envelop 
+	{
+		// Uses ConsoleRasteriser functions to draw the output grid
+		// eg. mConsoleRasteriser.drawLine(7, 9, -6) for a line starting at (7, 9) and going 10 spaces down.
+		
+	}
 	
 }
