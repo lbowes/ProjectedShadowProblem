ec88d6e94e3765eb16753606678d646213e0d151
Evan Snoswell <es1492@its.york.ac.uk>
2019-03-07 17:42:10 +0000

    Got rid of some stuff
    Signed-off-by: Evan Snoswell <es1492@its.york.ac.uk>

--- src/internal/Envelop.java
+++ src/internal/Envelop.java
@@ -2,11 +2,7 @@ package internal;
 
 public class Envelop 
 {
-	private int leftBound;
-	private int height;
-	private int rightBound;
-	
-	public Envelop(int left, int height, int right)
+	public Envelop(int[] verts)
 	{
 		
 	}
